d5b014c3584ed586c42b48d9ccc5e2e76ee31e38
pol_garcia <pol.garcia@basetis.com>
2020-12-29 12:24:02 +0100

    sdsd

--- encoder.py
+++ encoder.py
@@ -84,9 +84,6 @@ class Encoder:
     def encoder_loss(self):
         intermediate_layer_model = keras.Model(inputs=self.discriminator.input,
                                                outputs=self.discriminator.get_layer("feature_extractor").output)
-        print(intermediate_layer_model.summary())
-        print("---")
-        print(self.generator.summary())
 
         def loss(y_true, y_pred):
             l1 = K.mean(K.square(y_pred - y_true))
