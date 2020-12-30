4fb30ed0fc90b79e6219236053cba95843fcb798
pol_garcia <pol.garcia@basetis.com>
2020-12-30 09:16:07 +0100

    sdsd

--- encoder_trainer.py
+++ encoder_trainer.py
@@ -117,9 +117,7 @@ class Trainer:
         x_test = self.get_batch(32,False)
         y_test = self.regen_batch(x_test)
         score = self.encod.evaluate(x_test, y_test, verbose=0)
-        print(score)
         print('Test loss:', score[0])
-        print('Test accuracy:', score[1])
 
 
 if __name__ == '__main__':
@@ -127,3 +125,4 @@ if __name__ == '__main__':
     trainer = Trainer(encod)
     trainer.train()
     trainer.test()
+    trainer.encod.save('encoder.h5')
