2092386352b3f6e265bb7d8b15ba710e59decada
pol_garcia <pol.garcia@basetis.com>
2020-12-30 20:00:54 +0100

    sdsd

--- test.py
+++ test.py
@@ -65,7 +65,6 @@ if __name__ == '__main__':
     im = get_batch(1)
     im2 = generator.predict(encoder.predict(im))
     im2_res = im2.reshape([im2.shape[-1]//2,-1])
-    img = Image.fromarray(im, 'RGB')
     img2 = Image.fromarray(im2_res, 'RGB')
     img.save("image_real.jpg")
     img2.save("image_regen.jpg")
