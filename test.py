6a936c1b759bd4c60ed297d44a49d438e786231a
pol_garcia <pol.garcia@basetis.com>
2020-12-31 13:28:15 +0100

    sdsd

--- test.py
+++ test.py
@@ -73,10 +73,10 @@ if __name__ == '__main__':
         im2 = generator.predict(encoder.predict(im))
 
         plt.imshow(im2.squeeze(), cmap='gray')
-        plt.savefig('resultats_encoding/image_reconstructed'+i+'.png')
+        plt.savefig('resultats_encoding/image_reconstructed'+str(i)+'.png')
 
         plt.imshow(im.squeeze(), cmap='gray')
-        plt.savefig('resultats_encoding/image_real'+i+'.png')
+        plt.savefig('resultats_encoding/image_real'+str(i)+'.png')
 
 
 
