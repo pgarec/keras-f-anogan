322e78ac7de80f1c08a7d3004ae6f60fca9f7259
pol_garcia <pol.garcia@basetis.com>
2021-01-05 10:21:18 +0100

    sdsd

--- encoder.py
+++ encoder.py
@@ -60,7 +60,7 @@ class Encoder:
         model.add(BatchNormalization())
         model.add(Flatten())
         model.add(Dense(100, activation=custom_activation))
-        model.add(Reshape((1,1,100)))
+        model.add(Reshape((1,100)))
         '''model.add(Dense(1024, activation='relu', input_shape=(32,32,1)))
         model.add(Dense(512, input_shape=(1024,), activation='relu'))
         model.add(Dense(256, activation='relu', input_shape=(512,)))
