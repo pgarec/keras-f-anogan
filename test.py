1bb7a4f898b35aa3d5b2b03bdee7f0b3b4377f32
pol_garcia <pol.garcia@basetis.com>
2021-01-07 16:36:31 +0100

    sdsd

--- test.py
+++ test.py
@@ -28,6 +28,8 @@ def wasserstein_loss(y_true, y_pred):
 
 z_size=(1, 1, 100)
 discriminator = load_model('disc.h5', custom_objects={'wasserstein_loss': wasserstein_loss})
+disc_gp = load_model('disc-gp.h5')
+gen_gp = load_model('gen-gp.h5')
 encoder = load_model('encoder.h5', custom_objects={'loss':encoder_loss(), 'custom_activation':custom_activation})
 generator = load_model('gen.h5', custom_objects={'wasserstein_loss': wasserstein_loss})
 encodergen = load_model('encodergen.h5', custom_objects={'loss':encoder_loss(), 'custom_activation':custom_activation})
