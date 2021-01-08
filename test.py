b337385323d631f9942ac544fbbc064fd46c948c
pol_garcia <pol.garcia@basetis.com>
2021-01-08 17:19:11 +0100

    sdsd

--- test.py
+++ test.py
@@ -100,7 +100,6 @@ if __name__ == '__main__':
         print(a)
         n = make_noise(1)
         g = generator.predict(n)
-        axes.append(fig.add_subplot(rows, cols, a + 1))
         plt.imshow(g.squeeze())
 
     fig.savefig('collage.png')
