dce8ba0c7c90c46c880dd98ac379567da8112f33
pol_garcia <pol.garcia@basetis.com>
2021-01-09 17:45:57 +0100

    sdsd

--- test.py
+++ test.py
@@ -153,6 +153,7 @@ if __name__ == '__main__':
         loss_fake[i] = encoder_loss2(fake[i],fake_regen[i])
         loss_real[i] = encoder_loss2(real[i], real_regen[i])'''
 
+    print(loss_fake)
     print(np.mean(loss_fake))
     print(np.mean(loss_real))
 
